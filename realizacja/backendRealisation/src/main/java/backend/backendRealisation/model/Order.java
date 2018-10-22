8e14f67b3484acd5a03776188d0417e5d62f74cd
Witkowp <witkowskipiotr96@gmail.com>
2018-10-22 21:25:15 +0200

    Lista zamowien

---
+++ realizacja/backendRealisation/src/main/java/backend/backendRealisation/model/Order.java
@@ -0,0 +1,97 @@
+package backend.backendRealisation.model;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by Piotr on 2018-10-22.
+ */
+public class Order {
+
+    public Order(){}
+
+    public Order(int id,int clientId,int restaurantId,List<Course> courseList,int price,OrderStatus orderStatus,Date addingDate,int grade){
+        this.id=id;
+        this.clientId=clientId;
+        this.restaurantId=restaurantId;
+        this.courseList=courseList;
+        this.price=price;
+        this.orderStatus=orderStatus;
+        this.addingDate=addingDate;
+        this.grade=grade;
+    }
+
+    private int id;
+    private int clientId;
+    private int restaurantId;
+    private List<Course> courseList;
+    private int price;
+    private OrderStatus orderStatus;
+    private Date addingDate;
+    private int grade;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(int clientId) {
+        this.clientId = clientId;
+    }
+
+    public int getRestaurantId() {
+        return restaurantId;
+    }
+
+    public void setRestaurantId(int restaurantId) {
+        this.restaurantId = restaurantId;
+    }
+
+    public List<Course> getCourseList() {
+        return courseList;
+    }
+
+    public void setCourseList(List<Course> courseList) {
+        this.courseList = courseList;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return orderStatus;
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        this.orderStatus = orderStatus;
+    }
+
+    public Date getAddingDate() {
+        return addingDate;
+    }
+
+    public void setAddingDate(Date addingDate) {
+        this.addingDate = addingDate;
+    }
+
+    public int getGrade() {
+        return grade;
+    }
+
+    public void setGrade(int grade) {
+        this.grade = grade;
+    }
+}

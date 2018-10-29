d5fa7f07f5db6f4f06dc23674574b85af3839a07
Marek Noworolnik <mnoworolnik@bluesoft.net.pl>
2018-10-29 20:23:14 +0100

    Dodaje to co usuno Krystian

---
+++ oferty/oferty_db_mock.py
@@ -0,0 +1,28 @@
+def add_dish(restaurant_id, name, price, description):
+    pass
+
+
+def modify_dish(dish_id, restaurant_id, name, price, description):
+    pass
+
+
+def remove_dish(dish_id):
+    pass
+
+
+def get_restaurant_menu(restaurant_id):
+    menu = {"danie1": ["ładny opis", "20.01 zł"], "danie2": ["smaczne i zdrowe", "120.00 zł"]}
+    return menu
+
+
+def get_cities():
+    return {"Warszawa": 1, "Sierpc": 3, "Radom": 2}
+
+
+def get_restaurants_from_city(city_id):
+    if city_id == '1':
+        return ["Warszawska restauracja 1", "Warszawska restauracja 2"]
+    elif city_id == '2':
+        return ["Radomska restauracja 1", "Radomska restauracja 2"]
+    elif city_id == '3':
+        return ["Sierpcowe zakamarki", "Sierpcowe jadło"]

52d364bd3ad1d30b37b1334a4c527b54dddf38e5
Krystian Koziel <krystian.koziel02@gmail.com>
2018-10-29 20:48:47 +0100

    Added validation in logic

--- oferty/oferty_logic.py
+++ oferty/oferty_logic.py
@@ -18,15 +18,19 @@ def get_cities():
 @app.route('/cities/<city_id>', methods=['GET'])
 @cross_origin()
 def get_restaurants(city_id):
-    restaurants = oferty_db_mock.get_restaurants_from_city(city_id)
-    return jsonify({"restaurants": restaurants})
+    if isinstance(city_id, int):
+        restaurants = oferty_db_mock.get_restaurants_from_city(city_id)
+        return jsonify({"restaurants": restaurants})
+    return "City id is not int"
 
 
 @app.route('/restaurant-offer/<restaurant_id>', methods=['GET'])
 @cross_origin()
 def get_offer(restaurant_id):
-    menu = oferty_db_mock.get_restaurant_menu(restaurant_id)
-    return jsonify({"menu": menu})
+    if isinstance(restaurant_id, int):
+        menu = oferty_db_mock.get_restaurant_menu(restaurant_id)
+        return jsonify({"menu": menu})
+    return "Restaurant id is not int"
 
 
 if __name__ == "__main__":

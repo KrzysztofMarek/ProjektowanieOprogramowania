f40996dd6243ccbceea31880661284d35a1da9c0
Adam Kasperowicz <kadek750@gmail.com>
2018-11-03 17:44:31 +0100

    Walidacja dodania pracownika

--- zarzadzanie_personelem/zarzadzanie_personelem/src/main/java/zarzadzanie_personelem/Entity/EmployeeForm.java
+++ zarzadzanie_personelem/zarzadzanie_personelem/src/main/java/zarzadzanie_personelem/Entity/EmployeeForm.java
@@ -1,5 +1,6 @@
 package zarzadzanie_personelem.Entity;
 
+import java.util.function.IntConsumer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -51,12 +52,91 @@ public class EmployeeForm {
     }
 
     public void validate(){
-        if(telefon.length() != 9){
-            log.info("Telefon length is {} instead o 9", telefon.length());
+        
+        if(!validateName()){
             valid = false;
             return;
         }
-
+        if(!validateLastName()){
+            valid = false;
+            return;
+        }
+        if(!validateTelephone()){
+            valid = false;
+            return;
+        }
+        if(!validateLogin()){
+            valid = false;
+            return;
+        }
         valid = true;
+    }
+    
+    private boolean validateName(){
+        if(imie.length() > 20){
+            log.info("String {} has more than 20 signs", imie);
+            return false;            
+        }
+        if(!imie.chars().allMatch(Character::isLetter)){
+            log.info("Some signs of {} are not letters", imie);
+            return false;            
+        }
+        if(!Character.isUpperCase(imie.codePointAt(0))){
+            log.info("First letter of {} is not capitalized", imie);
+            return false;            
+        }
+        
+        if(!imie.substring(1).chars().allMatch(Character::isLowerCase)){
+            log.info("Letters after first one of {} are capitalized", imie);
+            return false;            
+        }
+        return true;
+    }
+    
+    private boolean validateLastName(){
+        if(nazwisko.length() > 30){
+            log.info("String {} has more than 30 signs", nazwisko);
+            return false;            
+        }
+        if(!nazwisko.chars().allMatch(Character::isLetter)){
+            log.info("Some signs of {} are not letters", nazwisko);
+            return false;            
+        }
+        return true;
+    }
+    
+    private boolean validateTelephone(){
+        if(!telefon.matches("[\\d+ ]+")){
+            log.info("Telephone number {} has illegal signs", telefon);
+            return false;            
+        }
+        String tmpPhone = telefon;
+        tmpPhone = tmpPhone.replaceAll(" ", "");
+        if(tmpPhone.codePointAt(0) == '+' && tmpPhone.length() != 12){
+            log.info("Telephone number with plus sign, {}, does not have 9 signs", telefon);
+            return false;            
+        }
+        Integer amountOfNumbers = tmpPhone.replaceAll("\\D", "").length();
+        if(amountOfNumbers != 9 && amountOfNumbers != 11 && amountOfNumbers != 13){
+            log.info("Telephone number , {}, has a different size than 9, 11 or 13", telefon);
+            return false;            
+        }
+        return true;
+    }
+    
+    private boolean validateLogin(){
+        if(login.length() > 20){
+            log.info("String {} has more than 20 signs", login);
+            return false;            
+        }
+        if(login.length() < 5){
+            log.info("String {} has less than 5 signs", login);
+            return false;            
+        }
+        if(!login.matches("[\\p{Graph}]+")){
+            log.info("Some signs of {} are not letters", login);
+            return false;            
+        }
+        return true;
     }
 }
--- zarzadzanie_personelem/zarzadzanie_personelem/src/test/java/zarzadzanie_personelem/Entity/EmployeeCreatorTest.java
+++ zarzadzanie_personelem/zarzadzanie_personelem/src/test/java/zarzadzanie_personelem/Entity/EmployeeCreatorTest.java
@@ -10,46 +10,372 @@ import org.powermock.modules.junit4.PowerMockRunner;
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(EmployeeCreator.class)
 public class EmployeeCreatorTest {
-    
+        
     
     @Test
-    public void testCreateEmployeeValid() throws Exception{
-        String employeeFormJson = "{\n" +
+    public void testCreateEmployeeNameCorrect() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
                                 "    \"id_restauracji\": \"1\", \n" +
-                                "    \"imie\": \"Adam\",\n" +
-                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"imie\": \"Adrian\",\n" +
+                                "    \"nazwisko\": \"Hasz\",\n" +
                                 "    \"telefon\": \"123456789\",\n" +
-                                "    \"stanowisko\": \"a\",\n" +
-                                "    \"login\": \"a\",\n" +
-                                "    \"haslo\": \"a\"\n" +
+                                "    \"stanowisko\": \"Kucharz\",\n" +
+                                "    \"login\": \"haszImasz\",\n" +
+                                "    \"haslo\": \"Pyth0n_3xp3rt!\"\n" +
                                 " } ";
-        EmployeeCreator employeeCreator = new EmployeeCreator();
-        EmployeeCreator employeeCreatorSpy = PowerMockito.spy(employeeCreator);
         
-        PowerMockito.doReturn("Success").when(employeeCreatorSpy, "saveEmployeeToDB", Mockito.any());
-        String result = employeeCreatorSpy.createEmployee(employeeFormJson, "");
+        result =helperValidator(employeeFormJson);
+        assertNotEquals(result, "Invalid input");   
+    }    
         
-        assertEquals(result, "Success");
-    }
-    
     @Test
-    public void testCreateEmployeeInvalid() throws Exception{
-        String employeeFormJson = "{\n" +
+    public void testCreateEmployeeNameFirstLetterUpperCase() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
                                 "    \"id_restauracji\": \"1\", \n" +
-                                "    \"imie\": \"Adam\",\n" +
+                                "    \"imie\": \"adam\",\n" +
                                 "    \"nazwisko\": \"Adam\",\n" +
                                 "    \"telefon\": \"1\",\n" +
                                 "    \"stanowisko\": \"a\",\n" +
                                 "    \"login\": \"a\",\n" +
                                 "    \"haslo\": \"a\"\n" +
                                 " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+    
+    @Test
+    public void testCreateEmployeeNameOnlyLetters() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam99\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam!\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+    
+    @Test
+    public void testCreateEmployeeNameMax20Letters() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adamadamadamadamadamadamadamadam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+        
+    }
+    
+    @Test
+    public void testCreateEmployeeLastNameOnlyLetters() throws Exception{
+        String result;
+        String employeeFormJson;        
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam99\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");        
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam!\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");        
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam adam\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+        
+    }
+    
+    @Test
+    public void testCreateEmployeeNameMax30Letters() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"AdamadamadamadamadamadamadamadamAdamadamadamadamadamadamadamadam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"1\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+        
+    }
+        
+    @Test
+    public void testCreateEmployeeTelephoneOnlyPhoneSigns() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"123abc123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+        
+    @Test
+    public void testCreateEmployeeTelephoneOnly9or11or13Signs() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"123 123 123 123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+        
+    @Test
+    public void testCreateEmployeeTelephoneWithPlusOnly11Signs() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"+ 48 123 123 123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"a\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+        
+    @Test
+    public void testCreateEmployeeTelephoneCorrect() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"+48123123123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"aaaaa\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertNotEquals(result, "Invalid input");
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"0048123123123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"aaaaa\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertNotEquals(result, "Invalid input");
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"123 123 123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"aaaaa\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertNotEquals(result, "Invalid input");
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"+48 1234 12345\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"aaaaa\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertNotEquals(result, "Invalid input");
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"00 48 123456789\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"aaaaa\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertNotEquals(result, "Invalid input");
+    }
+        
+    @Test
+    public void testCreateEmployeeLoginMax20Signs() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"+ 123 123 123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"Haslohaslohaslohaslo123\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+        
+    @Test
+    public void testCreateEmployeeLoginMin5Signs() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"+ 123 123 123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"Has1\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+        
+    @Test
+    public void testCreateEmployeeLoginNormalSigns() throws Exception{
+        String result;
+        String employeeFormJson;
+        
+        employeeFormJson = "{\n" +
+                                "    \"id_restauracji\": \"1\", \n" +
+                                "    \"imie\": \"Adam\",\n" +
+                                "    \"nazwisko\": \"Adam\",\n" +
+                                "    \"telefon\": \"+ 123 123 123\",\n" +
+                                "    \"stanowisko\": \"a\",\n" +
+                                "    \"login\": \"ąę\",\n" +
+                                "    \"haslo\": \"a\"\n" +
+                                " } ";
+        
+        result =helperValidator(employeeFormJson);
+        assertEquals(result, "Invalid input");
+    }
+    
+    
+    private String helperValidator(String formJson) throws Exception{
         EmployeeCreator employeeCreator = new EmployeeCreator();
         EmployeeCreator employeeCreatorSpy = PowerMockito.spy(employeeCreator);
         
-        PowerMockito.doReturn("Success").when(employeeCreatorSpy, "saveEmployeeToDB", Mockito.any());
-        String result = employeeCreatorSpy.createEmployee(employeeFormJson, "");
-        
-        assertEquals(result, "Invalid input");
+        PowerMockito.doReturn("Success").when(employeeCreatorSpy, "saveEmployeeToDB", Mockito.any(), Mockito.any());
+        String result = employeeCreatorSpy.createEmployee(formJson, "");
+        return result;
     }
     
 }

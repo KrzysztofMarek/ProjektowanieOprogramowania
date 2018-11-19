83f3ea4a012cbc17c7f169004061a91e7564c829
Cukier <mat.cichocki95@gmail.com>
2018-11-19 21:46:30 +0100

    wrzucenie backendu

--- zamowienia/backend/complete/src/main/java/ExternalServices/DbService.java
+++ zamowienia/backend/complete/src/main/java/ExternalServices/DbService.java
@@ -7,6 +7,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.io.UnsupportedEncodingException;
+import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,11 +17,14 @@ import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.client.utils.URIBuilder;
 import org.apache.http.entity.ContentType;
 import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.HttpParams;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -32,13 +36,13 @@ public class DbService {
     private String componentUrl;
 
     public DbService(){
-        this.componentUrl = ""; 
+        this.componentUrl = "http://127.0.0.1:5000/"; 
     }
 
     public Integer AddOrder(int clientId, int restaurantId, ArrayList<OrderItem> products, double price)
             throws UnsupportedOperationException, IOException {
         HttpClient httpclient = HttpClients.createDefault();
-        HttpPost httppost = new HttpPost(componentUrl);
+        HttpPost httppost = new HttpPost(componentUrl + "dodaj_zamowienie");
 
         JSONObject json = new JSONObject();
         json.put("id_klienta", clientId);
@@ -64,11 +68,19 @@ public class DbService {
         HttpResponse response = httpclient.execute(httppost);
         HttpEntity entity = response.getEntity();
 
+        int statusCode = response.getStatusLine().getStatusCode();
+
         if (entity != null) {
             try (InputStream instream = entity.getContent()) {
                 return Integer.parseInt(StringTools.convertStreamToString(instream));
             }
+        }else{
+            if(statusCode == 200){
+                return 0;
+            }
         }
+
+
         return null;
     }
 
@@ -84,9 +96,19 @@ public class DbService {
         return false;
     }
 
-    public ArrayList<Integer> GetPreviousOrdersIds(int clientId){
+    public ArrayList<Integer> GetPreviousOrdersIds(Integer clientId) throws URISyntaxException, ClientProtocolException, IOException {
         ArrayList<Integer> ordersIds = new ArrayList<Integer>();
         ordersIds.add(124412);
+        
+        URIBuilder builder = new URIBuilder(this.componentUrl);
+        builder.setParameter("id_klienta", clientId.toString());
+
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpGet request = new HttpGet(builder.build());
+
+        //Execute and get the response.
+        HttpResponse response = httpclient.execute(request);
+        HttpEntity entity = response.getEntity();
 
         return ordersIds;
     }

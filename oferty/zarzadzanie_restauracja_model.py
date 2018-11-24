532b5244da1956c321369c424f71acd47a585ae1
Marek Noworolnik <mnoworolnik@bluesoft.net.pl>
2018-11-24 17:06:42 +0100

    New dishes form with validation, host and ports update

--- oferty/zarzadzanie_restauracja_model.py
+++ oferty/zarzadzanie_restauracja_model.py
@@ -3,5 +3,5 @@ import requests
 
 
 def pobierz_menu_restauracji(id_restauracji):
-    menu_restauracji = requests.get(f'http://0.0.0.0:5000/pobierz_menu_restauracji?id_restauracji={id_restauracji}')
+    menu_restauracji = requests.get(f'http://localhost:5000/pobierz_menu_restauracji?id_restauracji={id_restauracji}')
     return json.loads(menu_restauracji.text)
--- oferty/zarzadzanie_siecia_model.py
+++ oferty/zarzadzanie_siecia_model.py
@@ -3,5 +3,5 @@ import requests
 
 
 def pobierz_menu_sieci():
-    menu_sieci = requests.get('http://0.0.0.0:5000/pobierz_menu_restauracji?id_restauracji=0')
+    menu_sieci = requests.get('http://localhost:5000/pobierz_menu_restauracji?id_restauracji=0')
     return json.loads(menu_sieci.text)

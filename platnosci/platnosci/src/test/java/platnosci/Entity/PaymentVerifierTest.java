735dd76cdc9c2d2060233dcb25300f110879386a
Adam Kasperowicz <kadek750@gmail.com>
2018-12-04 15:44:31 +0100

    Fixy

--- platnosci/platnosci/src/test/java/platnosci/Entity/PaymentVerifierTest.java
+++ platnosci/platnosci/src/test/java/platnosci/Entity/PaymentVerifierTest.java
@@ -4,19 +4,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class PaymentVerifierTest {
-    
-    @Test
-    public void testProcessValid() {
-        String paymentFormJson = "{\n" +
-                            "    \"id_klienta\": \"1\",\n" +
-                            "    \"id_zamowienia\": \"1\",\n" +
-                            "    \"sposób_zapłaty\": \"PAYPAL\"\n" +
-                            "  }";
-        PaymentVerifier paymentVerifier = new PaymentVerifier();
-        String result = paymentVerifier.process(paymentFormJson, null);
-        assertEquals(result, "https://localhost:4202/");
-    }
-    
+        
     @Test
     public void testProcessInvalid() {
         String paymentFormJson = "{\n" +

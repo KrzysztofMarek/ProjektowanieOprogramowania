39fbeec30f5c3383792bdacee4dee73bae23da6a
Marek Noworolnik <mnoworolnik@bluesoft.net.pl>
2018-12-08 21:56:40 +0100

    Added ports configuration, integration with Meneger UI

---
+++ oferty/front/config.py
@@ -0,0 +1,9 @@
+class Config:
+    DEBUG = True
+    FRONT_HOST = '0.0.0.0'
+    FRONT_PORT = 9999
+    BACKEND_HOST = 'localhost'
+    BACKEND_PORT = 8888
+    UI_HOST = 'localhost'
+    UI_SIEC_PORT = 4201
+    UI_RESTAURACJA_PORT = 4204
--- oferty/front/run.py
+++ oferty/front/run.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template
 from flask_cors import CORS, cross_origin
 
+from config import Config
 from oferta_sieci import oferta_sieci
 from oferta_restauracji import oferta_restauracji
 from formularz_potrawy_sieci import formularz_potrawy_sieci
 from formularz_potrawy_restauracji import formularz_potrawy_restauracji
 
 app = Flask(__name__)
+app.config.from_object(Config)
 cors = CORS(app)
 
 app.register_blueprint(oferta_sieci)
@@ -15,4 +17,4 @@ app.register_blueprint(formularz_potrawy_sieci)
 app.register_blueprint(formularz_potrawy_restauracji)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=9999, debug=True)
+    app.run(host=Config.FRONT_HOST, port=Config.FRONT_PORT)

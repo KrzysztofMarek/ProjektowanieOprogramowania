0996c67642e968e13e96b02ab2ec9294f17494c2
rafal0502 <rafalchojnacki0502@gmail.com>
2019-01-03 13:00:41 -0800

    integracja z zaslepka

--- logowanie_rejestracja/log_restaurant.py
+++ logowanie_rejestracja/log_restaurant.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 from flask import Flask, render_template, request, escape, g ,render_template, url_for, redirect, request, flash, session
 from flask_jwt import JWT, jwt_required
-from security import authenticate, userrole_mapping, username_mapping, userid_mapping
+import requests
+from security import authenticate
 from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app)
 app.secret_key = "key"
-
 
 
 @app.route('/', methods=['GET','POST'])
@@ -21,23 +21,33 @@ def waliduj():
         session.pop('user', None)
         login = request.form['login']
         password = request.form['password']
-        if authenticate(login, password):
-            session['user'] = request.form['login']
-            if userid_mapping.get(str(request.form['login'])).stanowisko == "pracownik kuchni":
-                return redirect(url_for("kucharz"))
-            elif userid_mapping.get(str(request.form['login'])).stanowisko == 'klient':
+        parameters = {"id_pracownika": login}
+        response = requests.get("http://127.0.0.1:5000/pobierz_pracownika", params=parameters)
+        data = response.json()
+        if "success" not in data:
+            if authenticate(login, password, data["login"], data["hasło"]):
+                session['user'] = request.form['login']
+                if data['stanowisko'] == "Pracownik kuchni":
+                    return redirect(url_for("kucharz"))
+                elif data['stanowisko'] == 'Dostawca':
+                    return redirect(url_for("dostawca"))
+                else:
+                    return redirect(url_for("menadzer"))
+        parameters_2 = {"id_klienta": login}
+        response_2 = requests.get("http://127.0.0.1:5000/pobierz_klienta", params=parameters_2)
+        data_2 = response_2.json()
+        if "success" not in data_2:
+            if authenticate(login, password, data_2["login"], data_2["hasło"]):
+                session['user'] = request.form['login']
                 return redirect(url_for("klient"))
-            elif userid_mapping.get(str(request.form['login'])).stanowisko == 'dostawca':
-                return redirect(url_for("dostawca"))
-            else:
-                return redirect(url_for("menadzer"))
+
     return render_template('login.html')
 
 
 @app.route('/widok_klient')
 def klient():
     if g.user:
-        return render_template('results_client.html')
+        return render_template('results_client.html',the_login=session['user'])
     return redirect(url_for('waliduj'))
 
 
--- logowanie_rejestracja/security.py
+++ logowanie_rejestracja/security.py
@@ -1,27 +1,30 @@
+from pip._vendor import requests
 from werkzeug.security import safe_str_cmp
 from user import User
 
-users = [
-  #  User(1, 'Kubica', 'formula1', "Radom", "51240149102", "robert@kwp.pl", "Dostawca"),
-  #  User(2, 'Klient', 'password', "Warszawa", "512412412", "kowalskit@kwp.pl", "Klient"),
-  #  User(3, 'Gessler', 'jedzenie', "Krakow", "742121392", "kucharka@gmail.com", "Pracownik kuchni"),
-  #  User(4, 'Boss', 'haslo', "Krakow", "313514125", "menadzer@gmail.com", "Menadżer restauracji")
-    User('janPan', 1, 'Jan', 'Nowak', '123456789', 'pracownik kuchni', 'soicrupogi'),
-    User('AAmen', 2, 'Andrzej', 'Adrianowski', '777888999', 'dostawca', '39084utco'),
-    User('KNow', 4, 'Kasia', 'Nowak', '333444555', 'menadzer restauracji', 'kKdPS'),
 
-]
+#
+# users = [
+#   #  User(1, 'Kubica', 'formula1', "Radom", "51240149102", "robert@kwp.pl", "Dostawca"),
+#   #  User(2, 'Klient', 'password', "Warszawa", "512412412", "kowalskit@kwp.pl", "Klient"),
+#   #  User(3, 'Gessler', 'jedzenie', "Krakow", "742121392", "kucharka@gmail.com", "Pracownik kuchni"),
+#   #  User(4, 'Boss', 'haslo', "Krakow", "313514125", "menadzer@gmail.com", "Menadżer restauracji")
+#     User('janPan', 1, 'Jan', 'Nowak', '123456789', 'pracownik kuchni', 'soicrupogi'),
+#     User('AAmen', 2, 'Andrzej', 'Adrianowski', '777888999', 'dostawca', '39084utco'),
+#     User('KNow', 4, 'Kasia', 'Nowak', '333444555', 'menadzer restauracji', 'kKdPS'),
+#
+# ]
+#
+#
+# username_mapping = {u.imie: u for u in users}
+# userid_mapping = {u.id_pracownika: u for u in users}
+# userrole_mapping = {u.stanowisko: u for u in users}
 
 
-username_mapping = {u.imie: u for u in users}
-userid_mapping = {u.id_pracownika: u for u in users}
-userrole_mapping = {u.stanowisko: u for u in users}
-
-
-def authenticate(id_pracownika, haslo):
-    user = userid_mapping.get(id_pracownika, None)
-    if user and safe_str_cmp(user.haslo, haslo):
+def authenticate(login, password, id_pracownika, haslo):
+    if safe_str_cmp(login, id_pracownika) and safe_str_cmp(password, haslo):
         return True
-
+    else:
+        return False
 
 
